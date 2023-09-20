c0852b61492329f424dd73354caf0e71e2458e4e
fnhg1ab0 <thinhduc12345@gmail.com>
2023-09-20 15:32:28 +0700

    demo styling

--- code/src/components/UI/Button/Button.js
+++ code/src/components/UI/Button/Button.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import styles from './Button.module.css';
 
-import './Button.css';
+// import styled from "styled-components";
+
+// use styled-components to style the button for only this component
+// const ButtonClick = styled.button`
+//     width: 100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #8b005d;
+//     color: white;
+//     background: #8b005d;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+//
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+//
+// const Button = props => {
+//   return (
+//     <ButtonClick type={props.type} onClick={props.onClick}>
+//       {props.children}
+//     </ButtonClick>
+//   );
+// };
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
+      <button type={props.type} className={styles.button} onClick={props.onClick}>
+        {props.children}
+      </button>
   );
 };
 
