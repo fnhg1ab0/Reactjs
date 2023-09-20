393b55384bf9cf95a7349d0c10753265d97216af
fnhg1ab0 <thinhduc12345@gmail.com>
2023-09-20 17:01:11 +0700

    investment

---
+++ code/src/components/FormInput/FormInput.js
@@ -0,0 +1,39 @@
+import styles from './FormInput.module.css';
+
+const FormInput = ({ label, ...rest }) => {
+ return(
+     <form className={styles.form}>
+         <div className={styles['input-group']}>
+             <p>
+                 <label htmlFor="current-savings">Current Savings ($)</label>
+                 <input type="number" id="current-savings" />
+             </p>
+             <p>
+                 <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+                 <input type="number" id="yearly-contribution" />
+             </p>
+         </div>
+         <div className={styles['input-group']}>
+             <p>
+                 <label htmlFor="expected-return">
+                     Expected Interest (%, per year)
+                 </label>
+                 <input type="number" id="expected-return" />
+             </p>
+             <p>
+                 <label htmlFor="duration">Investment Duration (years)</label>
+                 <input type="number" id="duration" />
+             </p>
+         </div>
+         <p className={styles.actions}>
+             <button type="reset" className={styles.buttonAlt}>
+                 Reset
+             </button>
+             <button type="submit" className={styles.button}>
+                 Calculate
+             </button>
+         </p>
+     </form>
+ );
+}
+export default FormInput;
