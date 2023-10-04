ffc4215e70c527627e2600c32e0912c1ca683c3b
fnhg1ab0 <thinhduc12345@gmail.com>
2023-10-04 18:00:57 +0700

    custom react hook function

--- code/src/hooks/use-counter.js
+++ code/src/hooks/use-counter.js
@@ -3,20 +3,21 @@ import {useEffect, useState} from "react";
 /**
  * Custom hook for counter
  * @param initialize
- * if initialize is 1, then counter will plus 1 every second
- * else if initialize is -1, then counter will minus 1 every second
+ * default value is true,
+ * if initialize is true, then counter will plus 1 every second,
+ * else if initialize is false, then counter will minus 1 every second
  * @returns {number}
  */
-const useCounter = (initialize = 1) => {
+const useCounter = (initialize = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevCounter) => initialize === 1 ? prevCounter + 1 : prevCounter - 1);
+            setCounter((prevCounter) => initialize ? prevCounter + 1 : prevCounter - 1);
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [initialize]);
 
     return counter;
 }
