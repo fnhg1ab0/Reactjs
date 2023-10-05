45c9a41ef24a77abd2d86e2f4bbc0313f7a7aae2
fnhg1ab0 <thinhduc12345@gmail.com>
2023-10-05 19:49:08 +0700

    forms user input

--- code/src/components/SimpleInput.js
+++ code/src/components/SimpleInput.js
@@ -1,15 +1,47 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const refInput = useRef();
+    const [stateInput, setStateInput] = useState('');
+    const [stateIsValid, setStateIsValid] = useState(true);
+
+    const changeHandler = (event) => {
+        setStateInput(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if (stateInput.trim() === '') {
+            setStateIsValid(false);
+            return;
+        }
+        setStateIsValid(true);
+        console.log('use state to store input value: ', stateInput);
+        console.log('use ref to store input value: ', refInput.current.value);
+        setStateInput('');
+        refInput.current.value = '';
+    }
+
+    const inputClasses = stateIsValid ? 'form-control' : 'form-control invalid';
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className={inputClasses}>
+                <label htmlFor='name'>Your Name</label>
+                <input
+                    ref={refInput}
+                    type='text'
+                    id='name'
+                    onChange={changeHandler}
+                    value={stateInput}
+                />
+                {!stateIsValid && <p className="error-text">Name must not be empty.</p>}
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
