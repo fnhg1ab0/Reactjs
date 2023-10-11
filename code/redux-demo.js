bf33493a398d060a99922905d86d033efb217c72
fnhg1ab0 <thinhduc12345@gmail.com>
2023-10-11 13:49:56 +0700

    redux demo

---
+++ code/redux-demo.js
@@ -0,0 +1,37 @@
+// import redux library
+// npm init -y
+// npm install redux
+
+const redux = require('redux');
+
+// create a reducer function
+const counterReducer = (state = {counter: 0}, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
+}
+
+// create a store
+const createStore = redux.createStore(counterReducer);
+
+// create a subscriber function
+const counterSubscriber = () => {
+    const latestState = createStore.getState();
+    console.log(latestState);
+}
+
+// subscribe to the store
+createStore.subscribe(counterSubscriber);
+
+// dispatch an action
+createStore.dispatch({type: ''});
+createStore.dispatch({type: 'increment'});
+createStore.dispatch({type: 'decrement'});
